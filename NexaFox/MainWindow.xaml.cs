557370d27ef0890f749325e84384deba02339c74
BlackBluee <kacperpe38@gmail.com>
2025-02-25 19:59:01 +0100

    Dodaj pliki projektów.

---
+++ NexaFox/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Web.WebView2.Core;
+
+namespace NexaFox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeWebView();
+        }
+        private async void InitializeWebView()
+        {
+            // Sprawdzamy, czy WebView2 jest zainstalowane
+            await webView.EnsureCoreWebView2Async(null);
+
+            // Ładujemy stronę internetową
+            webView.Source = new Uri("https://www.google.com");
+        }
+    }
+}
